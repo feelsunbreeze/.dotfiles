7d2dbbc673cd8c1046ff3b30ee4c87c391b66d57
Feel Sunbreeze <matthiassunbreeze@gmail.com>
2022-12-07 22:45:42 +0500

    Create start.sh

---
+++ Hyprland/Catppuccin/configs/gtk/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# usage: import-gsettings
+config="${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
+if [ ! -f "$config" ]; then exit 1; fi
+
+gnome_schema="org.gnome.desktop.interface"
+gsettings set "$gnome_schema" gtk-theme "Catppuccin-Mocha-Pink"
+gsettings set "$gnome_schema" icon-theme "Catppuccin-SE"
+gsettings set "$gnome_schema" cursor-theme "Catppuccin-Mocha-Pink-Cursors"
+gsettings set "$gnome_schema" font-name "Jetbrains Mono"
